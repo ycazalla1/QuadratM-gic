b8c50f67fea40a90e38b51b3abdd39ea451368d9
ycaza <ycaza@DESKTOP-T1UA6VD>
2025-03-18 21:42:30 +0100

    Quinta versió Possible versió final (amb comentaris)

--- src/main/java/com/mycompany/elsquadratsmagics/MainClass.java
+++ src/main/java/com/mycompany/elsquadratsmagics/MainClass.java
@@ -6,44 +6,64 @@ package com.mycompany.elsquadratsmagics;
 import java.util.Scanner;
 
 /**
- *
- * @author Bea i Yamila Hay que pedir al usuario los números y luego decir si es
- * una caja mágica o no
+ * Classe principal.
+ * És on es demana a l'usuari per les dimensions del quadrat i el seu contingut.
+ * @author Bea i Yamila
  */
 public class MainClass {
 
+    /**
+     * Mètode main
+     * @param args 
+     */
     public static void main(String[] args) {
 
+        // Declaració de l'escàner
         Scanner sc = new Scanner(System.in);
 
-        int num = 0;
-        String resposta, cadena, subCadena = "";
+        // Declaració de variables
+        String resposta;
+        // Controla si l'usuari vol modificar les dimensions
         boolean triarDimensions = false;
+        // Constant per les dimensions per defecte del quadrat
         final int DIMENSIONS_DEFECTE = 3;
 
+        // Creació d'una matriu amb les dimensions per defecte
         int[][] llistaNumeros = new int[DIMENSIONS_DEFECTE][DIMENSIONS_DEFECTE];
 
+        // Missatge per confirmar si vol modificar les dimensions
         System.out.print("Dimensions de la capsa per defecte: " + DIMENSIONS_DEFECTE
                     + "X" + DIMENSIONS_DEFECTE + "\nVols modificar les dimensions"
                     + "per defecte (S/N): ");
+        // Bucle per comprovar que la resposta és vàlida
         do {
             resposta = sc.nextLine();
 
+            /* 
+                Si la resposta no és "S" ni "N", es mostra un missatge d'error i
+                es torna a demanar la resposta
+            */
             if (!resposta.equalsIgnoreCase("S") && !resposta.equalsIgnoreCase("N")) {
                 System.err.print("ERROR. Resposta incorrecte."
                         + "\nTorna a ficar una resposta (S/N): ");
             } else if (resposta.equalsIgnoreCase("S")) {
+                // Si no, crida al mètode demanarDimensions()
                 llistaNumeros = demanarDimensions();
+                // triarDimensions es fica cert per sortir del bucle
                 triarDimensions = true;
             }
 
         } while (!triarDimensions);
 
+        // Crida al mètode per omplir la matriu
         omplirMatriu(llistaNumeros);
 
+        // Crea una instància de QuadratMagic
         QuadratMagic qm = new QuadratMagic();
+        // Crida al mètode esMagic() per verificar si és màgic
         boolean magic = qm.esMagic(llistaNumeros);
 
+        // Si magic es cert vol dir que és un quadrat magic; si no, no
         if (magic) {
             System.out.println("És un quadrat màgic.");
         } else {
@@ -52,58 +72,88 @@ public class MainClass {
 
     }
 
+    /**
+     * Mètode per demanar les dimensions del quadrat.
+     * @return Matriu amb les dimensions.
+     */
     public static int[][] demanarDimensions() {
 
-        int dimensions = 0;
+        // Declaracions de varibles
+        int dimensions = 0; // Variable per emmagatzemar les dimensions
+        // Matriu que es crearà amb les dimensions triades
         int[][] llistaNumeros;
+        // Controla si les dimensions introduïdes són correctes
         boolean dmsCorrecte = false;
 
+        // Es demana a l'usuari que introdueixi les dimensions
         System.out.print("Introdueix les dimensions del quadrat: ");
+        // Bucle per assegurar-se que l'usuari introdueix un número enter positiu
         do {
             Scanner sc = new Scanner(System.in);
             if (sc.hasNextInt()) {
+                // Si és enter guarda guarda el valor en la variable
                 dimensions = sc.nextInt();
                 if (dimensions <= 0) {
+                    // Si no és positiu surt un missatge d'error
                     System.err.print("ERROR. No és un nombre positiu."
                         + "\nTorna'l a introduïr: ");
                 } else {
+                    // Si és, surt del bucle
                     dmsCorrecte = true;
                 }
             } else {
+                // Si no és enter, surt un missatge d'error
                 System.err.print("ERROR. No és un nombre enter."
                         + "\nTorna'l a introduïr: ");
             }
         } while (!dmsCorrecte);
 
+        // Assigna a la matriu les dimensions especificades per l'usuari
         llistaNumeros = new int[dimensions][dimensions];
 
-        return llistaNumeros;
+        return llistaNumeros; // Es retorna la matriu
 
     }
     
+    /**
+     * Mètode per omplir la matriu.
+     * @param matriu Matriu que serà omplerta.
+     */
     public static void omplirMatriu(int[][] matriu) {
         
-        int num;
+        // Declaració de variables
+        int num; // Variable per emmagatzemar els nombres
+        // Controla si el nombre introduït és vàlid
         boolean numCorrecte = false;
         
+        // Primer "for" per recòrrer la matriu per files
         for (int i = 0; i < matriu.length; i++) {
+            // S'imprimeix la línia on estem situats
             System.out.println("--- Fila " + (i + 1) + " ---");
+            // Segon "for" per recòrrer la matriu per columnes
             for (int j = 0; j < matriu[i].length; j++) {
+                /* 
+                    Bucle per assegurar-se que l'usuari introdueix un número enter
+                    positiu
+                */
                 do {
                     Scanner sc = new Scanner(System.in);
                     System.out.print("Columna " + (j + 1) + " : ");
                     if (sc.hasNextInt()) {
+                        // Si és enter guarda guarda el valor en la variable
                         num = sc.nextInt();
                         if (num <= 0) {
+                            // Si no és positiu surt un missatge d'error
                             System.err.println("ERROR. No és un nombre positiu.");
                         } else {
+                            // Si és, surt del bucle
                             numCorrecte = true;
                         }
                     } else {
+                        // Si no és enter surt un missatge d'error
                         System.err.println("ERROR. No és un nombre enter.");
                     }
                 } while (!numCorrecte);
-                
                 
             }
         }
--- src/main/java/com/mycompany/elsquadratsmagics/QuadratMagic.java
+++ src/main/java/com/mycompany/elsquadratsmagics/QuadratMagic.java
@@ -5,47 +5,91 @@
 package com.mycompany.elsquadratsmagics;
 
 /**
+ * Classe QuadratMagic. Comprova si cert o fals que el quadrat és màgic.
  *
  * @author Bea i Yamila
  */
 public class QuadratMagic {
 
+    /**
+     * Constructor buit de la classe, ja que no en té d'atributs la classe
+     */
     public QuadratMagic() {
 
     }
 
+    /**
+     * Mètode que comprova si la matriu introduïda és un quadrat màgic.
+     *
+     * @param matriu Matriu a comprovar.
+     * @return Retorna true si és un quadrat màgic i false si no ho és.
+     */
     public boolean esMagic(int[][] matriu) {
 
-        boolean magic;
-
+        // Declaracions de variables
+        // Variable booleana per indicar si la matriu és màgica o no
+        boolean magic = true;
+        // Nombre de files i columnes de la matriu
         int n = matriu.length, l = matriu.length;
-        int sumaFiles[] = new int [n], sumaColumnes[] = new int [l];
+        // Arrays per emmagatzemar la suma de cada fila i columna
+        int sumaFiles[] = new int[n], sumaColumnes[] = new int[l];
+        // Variables per a les sumes de les diagonals
         int sumaDiago1 = 0, sumaDiago2 = 0;
 
+        /*
+            for per recòrrer la matriu per calcular les sumes de files, columnes
+            i diagonals
+         */
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                
+
+                // Suma dels valors de cada fila
                 sumaFiles[i] += matriu[i][j];
+                // Suma els valors de cada columna
                 sumaColumnes[j] += matriu[i][j];
 
-                
+                // Suma dels valors de la diagonal
                 if (i == j) {
+                    /*
+                        Suma quan veu que dues posicions de les files i columnes
+                        són iguals, és la manera d'anar recorrent la matriu en
+                        diagonal
+                     */
                     sumaDiago1 += matriu[i][j];
                 }
 
-                
+                // Suma dels valors de la diagonal invertida
                 if (i + j == n - 1) {
+                    /*
+                        En comptes de comprovar que siguin iguals, si sumem
+                        les dues posicions i comperem amb un contador a l'inversa
+                        es pot recòrrer una diagonal inversa
+                     */
                     sumaDiago2 += matriu[i][j];
                 }
             }
         }
 
+        // Emmagatzamem la suma de la primera fila per fer la comparació
         int suma = sumaFiles[0];
-        
-        magic = sumaColumnes[0] == suma && sumaDiago1 == suma &&
-                sumaDiago2 == suma;
 
-        return magic;
+        // Fa el recorregut de les dues arrays creades per realitzar la suma
+        for (int i = 0; i < n; i++) {
+            if (sumaFiles[i] != suma || sumaColumnes[i] != suma) {
+                /* 
+                    Si troba alguna fila o columna que no és igual el quadrat
+                    no serà un quadrat màgic
+                */
+                magic = false;
+            }
+        }
+
+        // El mateix però amb les diagonals
+        if (sumaDiago1 != suma || sumaDiago2 != suma) {
+            magic = false;
+        }
+
+        return magic; // Retorna el valor de magic
     }
 
 }
