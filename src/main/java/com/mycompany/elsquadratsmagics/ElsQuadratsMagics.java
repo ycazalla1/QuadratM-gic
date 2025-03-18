cc6894440fdbb2a340dd9c8ed02112925e85d30f
ycaza <ycaza@DESKTOP-T1UA6VD>
2025-03-18 13:34:10 +0100

    Primera versió

---
+++ src/main/java/com/mycompany/elsquadratsmagics/ElsQuadratsMagics.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.elsquadratsmagics;
+
+/**
+ *
+ * @author Bea i Yamila
+ */
+public class ElsQuadratsMagics {
+
+    public static void main(String[] args) {
+        
+    }
+}
